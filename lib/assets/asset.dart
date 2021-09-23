1d39508b8dff6c2d3f4ced251b9c67030cf462c3
Onur Bulut <10urbulut@gmail.com>
2021-09-24 00:59:31 +0300

    Edit

--- lib/assets/asset.dart
+++ lib/assets/asset.dart
@@ -1,6 +1,6 @@
 class Asset {
   static const logoSmall =
-      "https://insoftpro.com/wp-content/themes/bizbag/assets/images/logo.png";
+      "https://www.google.com.tr/url?sa=i&url=https%3A%2F%2Ftwitter.com%2Flogodotcom&psig=AOvVaw1FILS7DhyVr8EzABEH-6E9&ust=1632520489886000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIiO5fGKlvMCFQAAAAAdAAAAABAD";
 
   static const logoMedium =
       "https://www.google.com.tr/url?sa=i&url=https%3A%2F%2Ftwitter.com%2Flogodotcom&psig=AOvVaw1FILS7DhyVr8EzABEH-6E9&ust=1632520489886000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIiO5fGKlvMCFQAAAAAdAAAAABAD";
